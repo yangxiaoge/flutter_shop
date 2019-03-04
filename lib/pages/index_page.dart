0a29f5c10ced826e85e1fce2b6af080ffad827df
yangjianan <364804051@qq.com>
2019-03-04 13:14:26 +0800

    底部导航

--- lib/pages/index_page.dart
+++ lib/pages/index_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import '../constants/import.dart';
+import 'home_page.dart';
+import 'category_page.dart';
+import 'cart_page.dart';
+import 'member_page.dart';
 
 class IndexPage extends StatefulWidget {
   _IndexPageState createState() => _IndexPageState();
@@ -9,29 +13,31 @@ class IndexPage extends StatefulWidget {
 class _IndexPageState extends State<IndexPage> {
   int _currentIndex = 0;
 
-  List<BottomNavigationBarItem> _bottomItems;
+  final List<BottomNavigationBarItem> _bottomItems = [
+    BottomNavigationBarItem(icon: Icon(CupertinoIcons.home), title: Text('首页')),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.search), title: Text('分类')),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.shopping_cart), title: Text('购物车')),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.profile_circled), title: Text('会员中心')),
+  ];
 
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    _bottomItems = [
-      BottomNavigationBarItem(
-          icon: Icon(CupertinoIcons.home), title: Text('首页')),
-      BottomNavigationBarItem(
-          icon: Icon(CupertinoIcons.search), title: Text('分类')),
-      BottomNavigationBarItem(
-          icon: Icon(CupertinoIcons.shopping_cart), title: Text('购物车')),
-      BottomNavigationBarItem(
-          icon: Icon(CupertinoIcons.profile_circled), title: Text('会员中心')),
-    ];
-  }
+  final List<Widget> tabPages = [
+    HomePage(),
+    CategoryPage(),
+    CartPage(),
+    MemberPage()
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
         appBar: AppBar(
           title: Text(Constants.appName),
         ),
+        body: tabPages[_currentIndex],
         bottomNavigationBar: BottomNavigationBar(
           fixedColor: Theme.of(context).primaryColor,
           type: BottomNavigationBarType.fixed,
---
+++ lib/pages/member_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MemberPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('会员中心页面')),
+    );
+  }
+}
